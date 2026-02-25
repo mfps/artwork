de0f2f9bf3246eae22667ac648857459be6130c8
Frank Wille <f.s.wille@gmail.com>
2026-02-25 17:55:20 +0100

    git ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    appId: 'YOUR_APP_ID',
+    apiKey: "AIzaSyCG0juB-qvLVcKdyghb7V_qPHp6HrELJjA",
+    authDomain: "artwork-f7698.firebaseapp.com",
+    projectId: "artwork-f7698",
+    storageBucket: "artwork-f7698.firebasestorage.app",
+    messagingSenderId: "753555953577",
+    appId: "1:753555953577:web:84f8e2764e99b5f44cd6b8",
   },
 };
